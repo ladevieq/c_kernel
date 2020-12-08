36ea7b1ee009e89fac79f9109160710b08536e90
ladevieq <ladevie.quentin@gmail.com>
2020-12-08 20:27:58 +0100

    Working atapi disk discovery

--- k/atapi.c
+++ k/atapi.c
@@ -16,10 +16,6 @@
 // void *read_block(size_t lba);
 // Feel free to modify the proposed function prototypes.
 
-void init_ATAPI() {
-    discover_atapi_drive();
-}
-
 // Wait helper functions
 /* ATA specifies a 400ns delay after drive switching -- often
  * implemented as 4 Alternative Status queries. */
@@ -45,15 +41,18 @@ s32 is_atapi_drive(u16 bus, u8 drive) {
     };
     u8 drive_ATAPI_signature[4] = { '\0' };
 
-    drive_ATAPI_signature[0] = inb(ATA_REG_SECTOR_COUNT(drive));
-    drive_ATAPI_signature[1] = inb(ATA_REG_LBA_LO(drive));
-    drive_ATAPI_signature[2] = inb(ATA_REG_LBA_MI(drive));
-    drive_ATAPI_signature[3] = inb(ATA_REG_LBA_HI(drive));
+    select_drive            (bus, drive);
+    wait_device_selection   (bus);
 
-    return memcmp(drive_ATAPI_signature, ATAPI_signature , 4) == 0;
+    drive_ATAPI_signature[0] = inb(ATA_REG_SECTOR_COUNT(bus));
+    drive_ATAPI_signature[1] = inb(ATA_REG_LBA_LO(bus));
+    drive_ATAPI_signature[2] = inb(ATA_REG_LBA_MI(bus));
+    drive_ATAPI_signature[3] = inb(ATA_REG_LBA_HI(bus));
+
+    return memcmp(drive_ATAPI_signature, ATAPI_signature , sizeof(ATAPI_signature)) == 0;
 }
 
-void discover_atapi_drive() {
+void discover_atapi_drive(void) {
     outb(PRIMARY_DCR, SRST);
     outb(PRIMARY_DCR, INTERRUPT_DISABLE);
 
@@ -63,17 +62,24 @@ void discover_atapi_drive() {
     u16 ATA_buses[2] = { PRIMARY_REG, SECONDARY_REG };
     u8 drives[2]  = { ATA_PORT_MASTER, ATA_PORT_SLAVE };
 
-    for (size_t bus_index; bus_index < 2; bus_index++) {
+    for (size_t bus_index = 0; bus_index < 2; bus_index++) {
         u16 bus = ATA_buses[bus_index];
 
-        for (size_t drive_index; drive_index < 2; drive_index++) {
+        for (size_t drive_index = 0; drive_index < 2; drive_index++) {
             u8 drive = drives[drive_index];
 
-            select_drive            (bus, drive);
-            wait_device_selection   (bus);
             if (is_atapi_drive          (bus, drive)) {
-                printf ("Bus : %d\t Drive : %d\t is an ATAPI drive\n", bus, drive);
+                if (bus == PRIMARY_REG) {
+                    printf ("Primary bus\t Drive : %d\t is an ATAPI drive\n", drive);
+                } else {
+                    printf ("Secondary bus\t Drive : %d\t is an ATAPI drive\n", drive);
+                }
             }
         }
     }
+}
+
+
+void init_ATAPI() {
+    discover_atapi_drive();
 }
